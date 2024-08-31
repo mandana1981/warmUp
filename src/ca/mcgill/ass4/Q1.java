10522752696390c3ee361f895c0a77eadef9d2d3
mandana1981 <mandana_so@yahoo.com>
2024-08-31 11:03:38 -0400

    changes

--- src/ca/mcgill/ass4/Q1.java
+++ src/ca/mcgill/ass4/Q1.java
@@ -4,6 +4,9 @@ import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
+/*Write a program that opens a .txt, reads the contents of the file line by line, and prints the content of
+each line. To do this, you should look up how to use the BufferedReader or FileReader class.Remember to use the try and catch blocks to handle errors like trying to open an non-existent file. A sample file for
+testing file reading is found in the provided files as dictionary.txt.*/
 
 public class Q1 {
     public static void main(String[] args) {
--- src/ca/mcgill/ass4/Q2.java
+++ src/ca/mcgill/ass4/Q2.java
@@ -4,6 +4,9 @@ import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
+/*Modify the previous program so that it stores every line in an ArrayList of String objects. You have to
+properly declare an ArrayList to store the results, and use add to store every line that your program
+reads in the ArrayList.*/
 
 public class Q2 {
     public static void main(String[] args) {
